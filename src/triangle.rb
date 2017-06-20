920cb247524e3c6cd5053500f8f23d0c3d6e677f
Brian Peck <catbert321@gmail.com>
2017-06-19 20:42:57 -0700

    about exceptions 161/283

--- src/triangle.rb
+++ src/triangle.rb
@@ -14,7 +14,25 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  raise TriangleError if invalidTriangle(a, b, c)
+
+  return :equilateral if a == b && b == c
+
+  return :isosceles   if a == b || b == c || a == c
+
+  :scalene
+
+end
+
+def invalidTriangle(a, b, c)
+  # check for small numbers
+  return true if [a, b, c].min <= 0
+
+  # at least check that sides are probably fine
+  x, y, z = [a, b, c].sort
+  return true if x + y <= z
+
+  false
 end
 
 # Error class used in part 2.  No need to change this code.
